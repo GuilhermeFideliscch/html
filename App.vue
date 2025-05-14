4a7c661816183dce578e82137906fa60f4890281
Guilherme <168233537+GuilhermeFideliscch@users.noreply.github.com>
2025-05-14 20:33:56 -0300

    Update App.vue

--- App.vue
+++ App.vue
@@ -2,6 +2,7 @@
 export default{
   data(){
     return{
+      controller_table: 0,
       nome: "",
       idade: "",
       cabelo: "",
@@ -11,90 +12,67 @@ export default{
   methods:{
   salvar_pessoas(){
     this.pessoas.push({"nome": this.nome, "idade": this.idade, "cabelo": this.cabelo})
+  },
+  mudar_para_produtos(){
+    if(this.controller_table > 0){
+    this.controller_table --;
+    }
+  },
+  mudar_para_cliente(){
+    if(this.controller_table < 1){
+    this.controller_table ++;
+    }
+  }
   }
 }
-}
-
 </script>
 
 <template>
-  <div class="header-container">
-  <h1>Pessoas</h1>
-  <button type="button" class="btn btn-success botao" data-bs-toggle="modal" data-bs-target="#exampleModal">Add</button>
+ <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Menu</a>
+    <button class="btn btn-primary" @click="mudar_para_produtos">Produtos</button>
+    <button class="btn btn-primary"@click="mudar_para_cliente">Clientes</button>
   </div>
+</nav>
 
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      <label for="nome" class="form-label">Nome</label>
-        <input type="text" id="nome" name="nome" class="form-control" v-model="nome">
-        <label for="idade" class="form-label">Idade</label>
-        <input type="text" id="idade" name="idade" class="form-control col-4" v-model="idade">
-        <div class="input-group mb-3">
-      <label class="input-group-text" for="inputGroupSelect01">Cabelo</label>
-      <select class="form-select" id="inputGroupSelect01" v-model="cabelo">
-      <option value="1">cabeludo</option>
-      <option value="2">careca</option>
-      <option value="3">careca-cabeludo</option>
-      </select>
-</div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" @click="salvar_pessoas()">salvar</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-  <div v-if="pessoas.lenght == 0">
-    <img src="C:\Users\Unicesumar\Desktop\projetos\latest\src\assets/download.jpg" class="imagem">
-    <h1 class="registro">Sem registro</h1>
-  </div>
-
-<div v-else>
+<div v-if="controller_table == 1">
+    <a style="margin-left: 10px">Clientes</a>
+    <button class="btn btn-primary add" style="margin-left: 1200px">ADD</button>
     <table class="table">
     <thead>
     <tr>
     <th>nome</th>
-    <th>idade</th>
-    <th>cabelo</th>
     </tr>
     </thead>
     <tbody>
     <tr v-for="pessoa in pessoas">
     <td>{{pessoa.nome}}</td>
-    <td>{{pessoa.idade}}</td>
-    <td>{{pessoa.cabelo}}</td>
     </tr>
     </tbody>
     </table>
   </div>
 
+  <div v-if="controller_table == 0">
+    <a style="margin-left: 10px">Produtos</a>
+    <button class="btn btn-primary add ">ADD</button>
+    <table class="table">
+    <thead>
+    <tr>
+    <th>Produtos</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="pessoa in pessoas">
+    <td>{{pessoa.nome}}</td>
+    </tr>
+    </tbody>
+    </table>
+  </div>
 </template>
 
-<style scoped>
-.botao{
-  margin-top: 12px;
-  margin-left: 900px;
-  margin-right: 50px;
-}
-.imagem{
-  margin-left: 520px;
-  margin-top: 100px;
-}
-.registro{
-  margin-left: 555px;
-}
-.header-container {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-.header-container h1 {
-    margin: 0;
+<style>
+  a{
+    font-weight: bold;
   }
 </style>
